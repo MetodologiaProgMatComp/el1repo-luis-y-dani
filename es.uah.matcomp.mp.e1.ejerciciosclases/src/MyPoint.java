fdc9b4996afa4f5968d58d79ecc0c702d8c382bd
DaniLuVi <d.luque@edu.uah.es>
2024-02-25 13:47:41 +0100

    empezado otra clase nueva

--- es.uah.matcomp.mp.e1.ejerciciosclases/src/MyPoint.java
+++ es.uah.matcomp.mp.e1.ejerciciosclases/src/MyPoint.java
@@ -3,6 +3,8 @@ public class MyPoint {
     private int y = 0;
 
     public MyPoint() {
+        this.x = 0;
+        this.y = 0;
     }
 
     public MyPoint(int x, int y) {
@@ -25,23 +27,30 @@ public class MyPoint {
     public void setY(int y) {
         this.y = y;
     }
-    public int getXY()[2]{
-        return [x, y];
-
+    public int[] getXY(){
+        int[] xy = new int[2];
+        xy[0] = x;
+        xy[1] = y;
+        return xy;
     }
     public void setXY(int x, int y){
-        this.
+        this.x = x;
+        this.y = y;
     }
     public String toString(){
         return "(" + x + "," + y + ")";
     }
     public double distance(int x, int y) {
-
+        this.x = x;
+        this.y = y;
+        return Math.sqrt(Math.pow(x, 2) + Math.pow(y,2));
     }
     public double distance(MyPoint another) {
-
+        this.x = another.x;
+        this.y = another.y;
+        return Math.sqrt(Math.pow(another.x, 2) + Math.pow(another.y, 2));
     }
     public double distance() {
-
+        return Math.sqrt(x*x + y*y);
     }
 }
