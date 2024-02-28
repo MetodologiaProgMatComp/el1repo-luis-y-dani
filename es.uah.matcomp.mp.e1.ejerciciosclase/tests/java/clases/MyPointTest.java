094c4c2198a12dda09667b71aebdb222c139f123
DaniLuVi <d.luque@edu.uah.es>
2024-02-28 23:40:23 +0100

    añadidos algunos mains y tests mas

---
+++ es.uah.matcomp.mp.e1.ejerciciosclase/tests/java/clases/MyPointTest.java
@@ -0,0 +1,74 @@
+package clases;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MyPointTest {
+
+    @Test
+    void getX() {
+        MyPoint s = new MyPoint(3, 4);
+        assertEquals(3, s.getX(), 3);
+    }
+
+    @Test
+    void getY() {
+        MyPoint s = new MyPoint(3, 4);
+        assertEquals(4, s.getY(), 4);
+    }
+
+    @Test
+    void setX() {
+        MyPoint s = new MyPoint(3, 4);
+        assertDoesNotThrow(()-> s.setX(3));
+        assertEquals(3, s.getX(), "Este no es el valor de X que esperaba");
+    }
+
+    @Test
+    void setY() {
+        MyPoint s = new MyPoint(3, 4);
+        assertDoesNotThrow(()-> s.setY(4));
+        assertEquals(4, s.getY(), "Este no es el valor de Y que esperaba");
+    }
+
+    @Test
+    void getXY() {
+        MyPoint s = new MyPoint(3, 4);
+        assertEquals(3, s.getX(), 3);
+        assertEquals(4, s.getY(), 4);
+        assertEquals((3,4), s.getXY(),"Estas no son las coordenadas que esperaba");
+    }
+
+    @Test
+    void setXY() {
+        MyPoint s = new MyPoint(3, 4);
+        assertDoesNotThrow(()-> s.setXY(3, 4));
+        assertEquals(3, s.getX(), "Esta no es la coordenada X que se esperaba");
+        assertEquals(4, s.getY(), "Esta no es la coordenada Y que se esperaba");
+    }
+
+    @Test
+    void testToString() {
+        MyPoint s = new MyPoint(3, 4);
+        assertEquals("(0,0)", s.toString(), "Esta no era la salida por pantalla que se esperaba");
+    }
+
+    @Test
+    void distance() {
+        MyPoint s = new MyPoint(3, 4);
+
+        assertEquals()
+    }
+
+    @Test
+    void testDistance() {
+        MyPoint s = new MyPoint(3, 4);
+    }
+
+    @Test
+    void testDistance1() {
+        MyPoint s = new MyPoint(3, 4);
+
+    }
+}
