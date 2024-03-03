4bc2a3557fd092162cde202289bbc7d0040dfd66
luis-agustin-avila <158047827+luis-agustin-avila@users.noreply.github.com>
2024-03-03 18:22:11 +0100

    vhjk

---
+++ es.uah.matcomp.mp.e1.ejerciciosclase/src/MainMyLine.java
@@ -0,0 +1,10 @@
+package es.uah.matcomp.mp.e1.ejerciciosclase.src;
+import es.uah.matcomp.mp.e1.ejerciciosclase.src.clases.MyLine;
+
+public class MainMyLine {
+    MyLine m1 = new MyLine(0, 0, 3, 3);
+    System.out.println(m1);
+    MyLine m2 = new MyLine((0, 0));
+
+
+}
