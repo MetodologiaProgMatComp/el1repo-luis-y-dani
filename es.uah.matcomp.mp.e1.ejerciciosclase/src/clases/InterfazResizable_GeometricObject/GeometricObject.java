8d3b65e2a238b702e5c13c79a24f616de120831c
luis-agustin-avila <158047827+luis-agustin-avila@users.noreply.github.com>
2024-03-04 23:35:05 +0100

    vhjk

---
+++ es.uah.matcomp.mp.e1.ejerciciosclase/src/clases/InterfazResizable_GeometricObject/GeometricObject.java
@@ -0,0 +1,6 @@
+package es.uah.matcomp.mp.e1.ejerciciosclase.src.clases.InterfazResizable_GeometricObject;
+
+public interface GeometricObject {
+    public double getPerimeter();
+    public double getArea();
+}
---
+++ es.uah.matcomp.mp.e1.ejerciciosclase/src/clases/InterfazResizable_GeometricObject/Resizable.java
@@ -0,0 +1,5 @@
+package es.uah.matcomp.mp.e1.ejerciciosclase.src.clases.InterfazResizable_GeometricObject;
+
+public interface Resizable {
+    public void Resize(int percent);
+}
