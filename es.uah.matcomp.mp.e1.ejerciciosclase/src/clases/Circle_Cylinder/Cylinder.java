b63f2e8a991446a3681ef8b8cecbb0692d75c9f7
luis-agustin-avila <158047827+luis-agustin-avila@users.noreply.github.com>
2024-03-04 18:10:14 +0100

    vhjk

---
+++ es.uah.matcomp.mp.e1.ejerciciosclase/src/clases/Circle_Cylinder/Cylinder.java
@@ -0,0 +1,36 @@
+package es.uah.matcomp.mp.e1.ejerciciosclase.src.clases.Circle_Cylinder;
+import static java.lang.Math.PI;
+public class Cylinder extends Circle {
+    private double height;
+    public Cylinder(){
+        super();
+        this.height = 1.0;
+    }
+    public Cylinder(double height){
+        this.height = height;
+    }
+    public Cylinder(double radius, double height){
+        super(radius);
+        this.height = height;
+    }
+    public Cylinder(double radius, double height, String color){
+        super(radius, color);
+        this.height = height;
+    }
+    public double getHeight(){
+        return height;
+    }
+    public void setHeight(double height){
+        this.height = height;
+    }
+    public double getVolume(){
+        return getArea()*height;
+    }
+
+    @Override
+    public String toString() {
+        return "Cylinder: subclass of"+ super.toString()
+                + "height="+ height;
+    }
+}
+
