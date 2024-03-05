45f2ddf094f50b797788845337d28ba8dd5d2856
luis-agustin-avila <158047827+luis-agustin-avila@users.noreply.github.com>
2024-03-05 16:46:04 +0100

    vhjk

--- es.uah.matcomp.mp.e1.ejerciciosclase/tests/java/clases/Author_BookTest/AuthorTest.java
+++ es.uah.matcomp.mp.e1.ejerciciosclase/tests/java/clases/Author_BookTest/AuthorTest.java
@@ -1,4 +1,24 @@
 package es.uah.matcomp.mp.e1.ejerciciosclase.tests.java.clases.Author_BookTest;
-
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 public class AuthorTest {
+    Author a1 = new clases.AuthorBook.Author("Luis", "luis@gmail.com");
+    @Test
+    void getName(){
+        assertEquals(a1.getName(), "Luis");
+    }
+    @Test
+    void getEmail(){
+        assertEquals(a1.getEmail(), "luis@gmail.com");
+    }
+    @Test
+    void setEmail(){
+        a1.setEmail("luis@hotmail.com");
+        assertEquals(a1.getEmail(), "luis@hotmail.com");
+    }
+    @Test
+    void testToString(){
+        Author a2 = new clases.AuthorBook.Author("Avila", "avila@gmail.com");
+        assertEquals(a2.toString(), "Author[name=Avila, email=avila@gmail.com]");
+    }
 }
--- es.uah.matcomp.mp.e1.ejerciciosclase/tests/java/clases/Author_BookTest/BookTest.java
+++ es.uah.matcomp.mp.e1.ejerciciosclase/tests/java/clases/Author_BookTest/BookTest.java
@@ -1,4 +1,73 @@
 package es.uah.matcomp.mp.e1.ejerciciosclase.tests.java.clases.Author_BookTest;
+import clases.AuthorBook.Author;
+import clases.AuthorBook.Book;
+import org.junit.jupiter.api.Test;
+
+import javax.annotation.processing.SupportedAnnotationTypes;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class BookTest {
+    @Test
+    void getIsbn(){
+        Author Luis = new Author("Luis", "luis@gmail.com");
+        Book b1 = new Book("123", "CR7", Luis, 10.0);
+        assertEquals(b1.getIsbn(), "123");
+    }
+    @Test
+    void getName(){
+        Author Luis = new Author("Luis", "luis@gmail.com");
+        Book b1 = new Book("123", "CR7", Luis, 10.0);
+        assertEquals(b1.getName(), "CR7");
+    }
+    @Test
+    void getAuthor(){
+        Author Luis = new Author("Luis", "luis@gmail.com");
+        Book b1= new Book("123", "CR7", Luis, 10.0);
+        assertEquals(b1.getAuthor(), Luis);
+    }
+    @Test
+    void getPrice() {
+        Author Luis = new Author("Luis","luis@gmail.com");
+        Book b1 = new Book("123","CR7", Luis,10.0);
+        assertEquals(b1.getPrice(), 10.00);
+    }
+
+    @Test
+    void setPrice() {
+        Author Luis = new Author("Luis","luis@gmail.com");
+        Book b1 = new Book("123","CR7", Luis,10.0);
+        b1.setPrice(20.0);
+        assertEquals(b1.getPrice(), 20.0);
+    }
+
+    @Test
+    void getQty() {
+        Author Luis = new Author("Luis","luis@gmail.com");
+        Book b1 = new Book("123","CR7", Luis,10.0);
+        b1.setQty(10);
+        assertEquals(b1.getQty(), 10);
+    }
+
+    @Test
+    void setQty() {
+        Author Luis = new Author("Luis","luis@gmail.com");
+        Book b1 = new Book("123","CR7", Luis,10.0,20);
+        b1.setQty(10);
+        assertEquals(b1.getQty(), 10);
+    }
+
+    @Test
+    void getAuthorName() {
+        Author Luis = new Author("Luis","luis@gmail.com");
+        Book b1 = new Book("123","CR7", Luis,10.0,20);
+        assertEquals(b1.getAuthorName(),"Luis");
+    }
+
+    @Test
+    void testToString() {
+        Author Luis = new Author("Luis","luis@gmail.com");
+        Book b1 = new Book("123","CR7", Luis,10.0,20);
+        assertEquals(b1.toString(),"Book[isbn=123, name=CR7, Author[name=Luis, email=luis@gmail.com], price=10.0, qty=20]");
+    }
 }
