959680e4678816447ce1a031c7b19275fefea02a
luis-agustin-avila <158047827+luis-agustin-avila@users.noreply.github.com>
2024-03-05 14:38:37 +0100

    vhjk

---
+++ es.uah.matcomp.mp.e1.ejerciciosclase/tests/java/clases/RectangleTest.java
@@ -0,0 +1,56 @@
+package es.uah.matcomp.mp.e1.ejerciciosclase.tests.java.clases;
+import es.uah.matcomp.mp.e1.ejerciciosclase.src.clases.SuperclaseAbstractaShape.Rectangle;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+public class RectangleTest {
+    @Test
+    void getWidth(){
+        Rectangle rectangle = new Rectangle();
+        float w = (float) rectangle.getWidth();
+        assertNotEquals(w, 0L);
+        assertTrue(w>0L);
+    }
+    @Test
+    void getLength(){
+        Rectangle rectangle = new Rectangle();
+        float l = (float) rectangle.getLength();
+        assertNotEquals(l, 0L);
+        assertTrue(l>0L);
+    }
+    @Test
+    void setWidth(){
+        Rectangle rectangle = new Rectangle();
+        rectangle.setWidth(10.0f);
+        float a = (float) rectangle.getWidth();
+        assertEquals(a, 10.0f);
+        assertDoesNotThrow(() -> rectangle.setWidth(10.0f));
+    }
+    @Test
+    void setLength(){
+        Rectangle rectangle = new Rectangle();
+        rectangle.setLength(20.0f);
+        float b = (float) rectangle.getLength();
+        assertEquals(b, 20.0f);
+        assertDoesNotThrow(() -> rectangle.setLength(20.0f));
+    }
+    @Test
+    void getArea(){
+        Rectangle rectangle = new Rectangle(1.0f, 2.0f);
+        assertEquals(rectangle.getArea(), 2.0f);
+        assertTrue(rectangle.getArea()>0);
+        assertDoeNotThrow(() -> rectangle.getArea());
+    }
+    @Test
+    void getPerimeter(){
+        Rectangle rectangle = new Rectangle(5.0f, 10.0f);
+        assertEquals(30.0f, rectangle.getPerimeter());
+        assertDoesNotThrow(() -> rectangle.getPerimeter());
+    }
+    @Test
+    void testToString(){
+        Rectangle rectangle = new Rectangle(5.0f, 10.0f);
+        assertEquals("Rectangle[width = 5.0, length=10.0]", rectangle.toString());
+    }
+
+
+}
