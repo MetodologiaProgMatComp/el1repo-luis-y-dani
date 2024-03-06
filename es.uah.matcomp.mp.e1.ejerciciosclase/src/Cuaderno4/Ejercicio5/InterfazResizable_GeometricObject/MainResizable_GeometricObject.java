0c7f5d9858e0610332154377eae99656f9aeee82
luis-agustin-avila <158047827+luis-agustin-avila@users.noreply.github.com>
2024-03-06 23:31:02 +0100

    vhjk

--- es.uah.matcomp.mp.e1.ejerciciosclase/src/Cuaderno4/Ejercicio5/InterfazResizable_GeometricObject/MainResizable_GeometricObject.java
+++ es.uah.matcomp.mp.e1.ejerciciosclase/src/Cuaderno4/Ejercicio5/InterfazResizable_GeometricObject/MainResizable_GeometricObject.java
@@ -2,9 +2,13 @@ package Cuaderno4.Ejercicio5.InterfazResizable_GeometricObject;
 
 public class MainResizable_GeometricObject {
     public static void main(String[] args){
-
-
-
-
+        GeometricObject c1 = new Circle(4);
+        System.out.println(c1);
+        System.out.println("Area= "+c1.getArea());
+        System.out.println("Perimeter= "+c1.getPerimeter());
+        Resizable c2 = new ResizableCircle(2);
+        System.out.println(c2);
+        c2.Resize(50);
+        System.out.println("C2 new size:" + c2);
     }
 }
